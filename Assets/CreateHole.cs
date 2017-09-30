f633514d6d907008487469f99a14e6bce7913e49
vjifcu <victorjifcu@gmail.com>
2017-09-30 19:45:29 -0400

    7:45pm

--- Assets/CreateHole.cs
+++ Assets/CreateHole.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateHole : MonoBehaviour {
+public class CreateHole : Triggerable {
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +13,6 @@ public class CreateHole : MonoBehaviour {
 	void Update () {
 		
 	}
+
+
 }
