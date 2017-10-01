b581378a1887b12b4770f899344a2e78d3c52e14
vjifcu <victorjifcu@gmail.com>
2017-10-01 14:01:02 -0400

    2pm

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+
+    public AudioClip music;
+    private AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+        source.loop = true;
+        source.clip = music;
+        source.Play();
+	}
+
+}
